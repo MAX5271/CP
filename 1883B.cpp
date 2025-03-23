5c5562905d06f0aa91220defffcfd8ea1f1094c8
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-03-23 19:50:11 +0530

    1878C,1883B

---
+++ 1883B.cpp
@@ -0,0 +1,137 @@
+// template for cp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N=1e5+10;
+long long int arr[N];
+
+
+#define all(a) a.begin(),a.end() 
+
+ll int npr(ll int n,ll int r)
+{
+    r=min(r,(n-r));
+    ll int a=1;
+    for(ll int i=0;i<r;i++){
+        a*=(n-i);
+    }
+    for(ll int i=1;i<r;i++){
+        a/=(i+1);
+    }
+    return a;
+}
+
+vector <ll int> seive;
+
+bool prime[1000001];
+vector <ll> v1(1000001);
+void SieveOfEratosthenes(int n)
+{
+	memset(prime, true, sizeof(prime));
+ 
+	for (int p = 2; p * p <= n; p++) {
+		if (prime[p] == true) {
+			for (int i = p * p; i <= n; i += p){
+				if(v1[i]==0)   v1[i]=p;
+				prime[i] = false;
+			}
+		}
+	}
+	for (int p = 2; p <= n; p++)
+		if (prime[p])
+			seive.push_back(p);
+}
+
+void print(vector<ll int>& a){
+    for(auto element: a) cout<<element<<" ";
+    cout<<endl;
+}
+
+bool is_Prime(ll int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(ll int *a,ll int *b)
+{
+    ll int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll int factorial(ll int n)
+{
+    ll int p = 1;
+    for(int i=1;i<=n;i++)
+        p *= i;
+
+    return p;
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void solve()
+{
+   int n,k;
+   cin>>n>>k;
+   string s;
+   cin>>s;
+   int r=n-k;
+  
+   unordered_map <char,int> a;
+    
+   for(int i=0;i<n;i++){
+    a[s[i]]++;
+   }
+   int f=r;
+   if(r&1) r--;
+  
+    for(auto it = a.begin();it!=a.end();it++){
+        if(it->second>=r){
+            cy
+            return;
+        }else{
+            if(it->second&1){
+                r-=(it->second-1);
+            }else {
+                r-=(it->second);
+            }
+        }
+    }
+    if(r>0) cn
+    else cy
+   
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    
+}
