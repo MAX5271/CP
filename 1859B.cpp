ed55f8f5b239fca64b466d14b562e5301f8ffff6
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-03-28 20:35:06 +0530

    1849B,1859B

---
+++ 1859B.cpp
@@ -0,0 +1,143 @@
+// template for cp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N = 1e5 + 10;
+int arr[N];
+
+
+#define all(a) a.begin(), a.end()
+
+int npr(int n, int r)
+{
+    r = min(r, (n - r));
+    int a = 1;
+    for (int i = 0; i < r; i++) {
+        a *= (n - i);
+    }
+    for (int i = 1; i < r; i++) {
+        a /= (i + 1);
+    }
+    return a;
+}
+
+vector < int > seive;
+
+bool prime[1000001];
+vector < int > v1(1000001);
+void SieveOfEratosthenes(int n)
+{
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p) {
+                if (v1[i] == 0) v1[i] = p;
+                prime[i] = false;
+            }
+        }
+    }
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            seive.push_back(p);
+}
+
+void print(vector < int > & a) {
+    for (auto element: a) cout << element << " ";
+    cout << endl;
+}
+
+bool is_Prime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(int * a, int * b)
+{
+    int temp = * a;
+    * a = * b;
+    * b = temp;
+}
+
+int factorial(int n)
+{
+    int p = 1;
+    for (int i = 1; i <= n; i++)
+        p *= i;
+
+    return p;
+}
+
+void p(vector<vector<int>> a){
+    for(int i=0;i<a.size();i++) print(a[i]);
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void solve()
+{
+    int n,m;
+    cin>>n;
+    vector<vector<int>> a;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        vector<int> temp(m);
+        for(int j=0;j<m;j++){
+            cin>>temp[j];
+        }
+        sort(all(temp));
+        a.push_back(temp);
+    }
+    if(n==1){
+        cout<<a[0][0]<<endl;
+        return;
+    }
+    sort(all(a));
+    int mi=INT_MAX;
+    for(int i=0;i<n;i++){
+        mi=min(mi,a[i][1]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=a[i][1];
+    }
+    // cout<<sum<<" "<<mi<<" "<<a[0][0]<<endl;
+    sum=sum-mi+a[0][0];
+    cout<<sum<<endl;
+   
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+}
