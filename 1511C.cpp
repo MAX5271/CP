15f603a15ef0cec703ccdbc6677dd604fde8fc37
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-04-08 17:31:22 +0530

    1511C,1610B,1618C,1631B

---
+++ 1511C.cpp
@@ -0,0 +1,142 @@
+// template for cp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define MOD 1e9 + 7
+const int N = 1e5 + 10;
+int arr[N];
+
+
+#define all(a) a.begin(), a.end()
+
+int npr(int n, int r)
+{
+    r = min(r, (n - r));
+    int a = 1;
+    for (int i = 0; i < r; i++) {
+        a *= (n - i);
+    }
+    for (int i = 1; i < r; i++) {
+        a /= (i + 1);
+    }
+    return a;
+}
+
+vector < int > seive;
+
+bool prime[1000001];
+vector < int > v1(1000001);
+void SieveOfEratosthenes(int n)
+{
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p) {
+                if (v1[i] == 0) v1[i] = p;
+                prime[i] = false;
+            }
+        }
+    }
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            seive.push_back(p);
+}
+
+void print(vector < int > & a) {
+    for (auto element: a) cout << element << " ";
+    cout << endl;
+}
+
+void sortfdsi(vector<pair<int, int>>& vec){
+    sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        // Sort by first element in decreasing order
+        if (a.first != b.first) {
+            return a.first > b.first;
+        }
+        // If the first elements are equal, sort by the second element in increasing order
+        return a.second < b.second;
+    });
+
+}
+
+bool is_Prime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+void swap(int * a, int * b)
+{
+    int temp = * a;
+    * a = * b;
+    * b = temp;
+}
+
+int factorial(int n)
+{
+    int p = 1;
+    for (int i = 1; i <= n; i++)
+        p *= i;
+
+    return p;
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void solve()
+{
+    int n,q;
+    cin>>n>>q;
+    vector<int> a(n),b(q),p(51,-1);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<q;i++) cin>>b[i];
+    for(int i=0;i<n;i++){
+        if(p[a[i]]==-1)
+        p[a[i]]=i+1;
+    }
+    vector<int> ans(q);
+    for(int i=0;i<q;i++){
+        ans[i]=p[b[i]];
+        for(int j=1;j<=50;j++){
+            if(p[j]!=-1&&p[j]<ans[i]){
+                p[j]++;
+            }
+        }
+        p[b[i]]=1;
+    }
+    print(ans);
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+}
